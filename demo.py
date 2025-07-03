dffb1fcbc6f99142fbdd1e72e80238d797c50721
ritik Sahu <ritiksahu600@gmail.com>
2025-07-03 18:26:15 +0530

    adding the basic things

--- demo.py
+++ demo.py
@@ -1,1 +1,31 @@
+import random
 print("hello world")
+
+# variable declaration
+name ="Ritik"
+age =20
+# using two different data type variables,
+# we use comma to sperate the diferent different data type variable
+print(name +" " , age)
+
+#using same name data type variables
+surname =" sahu"
+print(name + surname)
+mob =9399109853
+print(age , mob)
+
+# different different variables have same value
+x=y=z="sahuji"
+print(x+" "+y+" "+z)
+
+## checking which data type is
+print(type(surname))
+
+#type conversion
+b=str(age)
+print(type(b))
+
+
+
+#random number
+print(random.randrange(50,60))
